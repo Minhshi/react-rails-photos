aa94c5f223be8fb5ce1124ecf166e0317410147c
Minh Pham <mpham@naver.com>
2019-03-10 08:15:40 +0100

    Add some styling to Card component

--- app/javascript/cards/components/Card.js
+++ app/javascript/cards/components/Card.js
@@ -2,7 +2,19 @@ import React from "react";
 
 class Card extends React.Component {
   render() {
-    return <div>Title: {this.props.card.title}</div>;
+    return (
+      <div className="ui card">
+        <div className="image">
+          <img
+            src="https://images.unsplash.com/photo-1490380169520-0a4b88d52565?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="header">{this.props.card.title}</div>
+        </div>
+      </div>
+    );
   }
 }
 
