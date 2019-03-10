7c7769a8dbe082c5ef97f48e0df333c821a7b72c
Minh Pham <mpham@naver.com>
2019-03-10 08:37:38 +0100

    Integrate react router dom in App component

--- app/javascript/cards/components/App.js
+++ app/javascript/cards/components/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import CardsIndex from "./CardsIndex";
 
 class App extends React.Component {
   render() {
     return (
       <div className="ui container">
-        <CardsIndex />
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={CardsIndex} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
