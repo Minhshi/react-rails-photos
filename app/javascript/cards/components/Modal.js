5bf764f3be72aedcb72174b5a63715641e6ac2fb
Minh Pham <mpham@naver.com>
2019-03-17 14:43:11 +0100

    Add some styling to Model component

--- app/javascript/cards/components/Modal.js
+++ app/javascript/cards/components/Modal.js
@@ -5,7 +5,14 @@ class Modal extends React.Component {
   render() {
     return ReactDOM.createPortal(
       <div className="ui dimmer modals visible active">
-        <div className="ui standard modal visible active">Modal</div>
+        <div className="ui standard modal visible active">
+          <div className="header">Delete Card</div>
+          <div className="content">Do you want to delete this card?</div>
+          <div className="actions">
+            <button className="ui button">Cancel</button>
+            <button className="ui button red">Delete</button>
+          </div>
+        </div>
       </div>,
       document.getElementById("modal")
     );
