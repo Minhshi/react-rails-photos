e158601d3a72d91820d9b7996b8bfd1c38cd95bd
Minh Pham <mpham@naver.com>
2019-03-17 15:19:22 +0100

    Add onClick event handlers for UI improvement

--- app/javascript/cards/components/Modal.js
+++ app/javascript/cards/components/Modal.js
@@ -1,11 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { history } from "./App";
 
 class Modal extends React.Component {
+  onModalBackgroundClick = () => {
+    history.push("/");
+  };
+
+  onModalClick = event => {
+    event.stopPropagation();
+  };
+
   render() {
     return ReactDOM.createPortal(
-      <div className="ui dimmer modals visible active">
-        <div className="ui standard modal visible active">
+      <div
+        className="ui dimmer modals visible active"
+        onClick={this.onModalBackgroundClick}
+      >
+        <div
+          className="ui standard modal visible active"
+          onClick={this.onModalClick}
+        >
           <div className="header">Delete Card</div>
           <div className="content">Do you want to delete this card?</div>
           <div className="actions">
